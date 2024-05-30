f2f3c695ef6546a41ce7e37f43ccc1466a38fd1b
Max Zayats <zmr2k17@gmail.com>
2024-05-30 11:11:52 +0300

    second final version of app

--- src/other/client/first_program/inputTextfield.java
+++ src/other/client/first_program/inputTextfield.java
@@ -5,7 +5,7 @@ import javax.swing.*;
 public class inputTextfield {
     public static void main(String[] args) {
         String text;
-        text= JOptionPane.showInputDialog("enter text");
+        text = JOptionPane.showInputDialog("enter text");
         JOptionPane.showMessageDialog(null, "you input text:\n "+text);
     }
 }
